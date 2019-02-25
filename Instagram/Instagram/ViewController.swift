c6e2b761c9421d65f48279c403c048ad568eb167
Ariel-McCarthy <somccarthy@csumb.edu>
2019-02-25 12:53:01 -0800

    added assets

---
+++ Instagram/Instagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Ariel McCarthy on 2/25/19.
+//  Copyright © 2019 Sovreign-Ariel McCarthy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
