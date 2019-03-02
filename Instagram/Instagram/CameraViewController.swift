c30b0cc05073e93ba7381c050e6c7e7fdb1df3f7
Ariel-McCarthy <somccarthy@csumb.edu>
2019-03-01 19:20:30 -0800

    Added a camera view controller with a tap gesture to choose photo

---
+++ Instagram/Instagram/CameraViewController.swift
@@ -0,0 +1,56 @@
+//
+//  CameraViewController.swift
+//  Instagram
+//
+//  Created by Ariel McCarthy on 3/1/19.
+//  Copyright © 2019 Sovreign-Ariel McCarthy. All rights reserved.
+//
+
+import UIKit
+
+class CameraViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var commentField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onSubmitButton(_ sender: Any)
+    {
+        
+    }
+    
+    @IBAction func onCameraTap(_ sender: Any)
+    {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.allowsEditing = true
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera)
+        {
+            picker.sourceType = .camera
+        }
+        else
+        {
+            picker.sourceType = .photoLibrary
+        }
+        present(picker, animated: true, completion: nil)
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
